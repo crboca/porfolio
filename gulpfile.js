bf2c80b07260ae584e5de507552109e9bddcf209
Cristina Boix <cristinaboixcanut@gmail.com>
2016-03-14 20:41:58 +0100

    updat gulp files

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+
+gulp.task('css',function(){
+	return gulp.src('src/scss/main.scss')
+	.pipe(sass())
+	.pipe(autoprefixer({
+		browsers: [
+		'last 2 versions',
+		'> 1%',
+		'ie 9'
+		]
+	}))
+	.pipe(gulp.dest('dist/css'));
+});
+
+gulp.task('default', ['css'], function(){
+	gulp.watch('src/scss/**/*.scss', ['css']);
+});
+
